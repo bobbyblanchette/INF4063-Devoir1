938a079a199adce5f0e7aae956864485668eaedc
Shushan <noname320@hotmail.com>
2016-10-31 13:33:55 -0400

    Initial commit.

---
+++ src/Devoir1.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Devoir1 
+{
+
+	public static void main(String[] args) 
+	{
+		if (args.length == 1)
+		{
+			readFile(args[0]);
+		}
+		else
+		{
+			System.out.println("Argument invalide.");
+		}
+		
+	}
+	
+	public static void readFile(String path)
+	{
+		try (BufferedReader br = new BufferedReader(new FileReader(path)))
+		{
+			int nbOfProblems = Integer.parseInt(br.readLine());
+			
+			for (int i = 0; i < nbOfProblems; i++)
+			{
+				br.skip(2);
+				String[] coinsStr = br.readLine().split("\\s+");
+				List<Integer> coins = Arrays.stream(coinsStr).mapToInt(Integer::parseInt).boxed().collect(Collectors.toList());
+				int total = Integer.parseInt(br.readLine());
+				new Problem(coins, total, i+1);
+			}
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+		}
+		catch(NumberFormatException e)
+		{
+			System.out.println("The input is in a wrong format");
+			e.printStackTrace();
+		}
+	}
+
+}
+
+
---
+++ src/Problem.java
@@ -0,0 +1,137 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Problem 
+{
+	int total;
+	List<Integer> coins;
+	List<Combination> solutions;
+	
+	public Problem(List<Integer> coins, int total, int pbNumber)
+	{
+		this.coins = coins;
+		this.total = total;
+		System.out.println("Cas " + pbNumber + " :");
+		solve();
+	}
+	
+	private int countOccurances(List<Integer> array, int n)
+	{
+		int result = 0;
+		for (int val : array)
+		{
+			if (val == n)
+				result++;
+		}
+		return result;
+	}
+	
+	public void printOptimalResult()
+	{
+		Combination result = solutions.get(0);
+		System.out.println(result.array.size() + " pièces utilisées");
+		for (int j = 0; j < coins.size(); j++)
+		{
+			if (result.array.contains(coins.get(j)))
+				System.out.print(countOccurances(result.array, coins.get(j)) + " fois " + coins.get(j) + ", ");
+		}
+		System.out.println();
+	}
+	
+	private void addToSolutions(Combination comb)
+	{
+		if (this.solutions == null)
+		{
+			this.solutions = new ArrayList<Combination>();
+			this.solutions.add(comb);
+			this.printOptimalResult();
+		}
+		else
+			this.solutions.add(comb);
+	}
+	
+	public void solve()
+	{
+		List<Combination> combTab = new ArrayList<Combination>();
+		for (int i = 0; i < coins.size(); i++)
+		{
+			if (coins.get(i) > total)
+			{
+				this.coins = coins.subList(0, i);
+				break;
+			}
+			else
+			{
+				Combination tempComb = new Combination(i);
+				if(tempComb.sum == total)
+					this.addToSolutions(tempComb);
+				if(tempComb.sum + coins.get(tempComb.maxIndex) <= total)
+					combTab.add(tempComb);
+			}
+		}
+		for (int n = 1; n < (total / coins.get(0)); n++)
+		{
+			List<Combination> nextCombTab = new ArrayList<Combination>();
+			for (Combination comb : combTab)
+			{
+				for (int i = comb.maxIndex; i < coins.size(); i++)
+				{
+					Combination tempComb = new Combination(comb);
+					tempComb.add(i);
+					if (tempComb.sum > total)
+						break;
+					else
+					{
+						if (tempComb.sum == total)
+						{
+							this.addToSolutions(tempComb);
+						}
+						if(tempComb.sum + coins.get(tempComb.maxIndex) <= total)
+							nextCombTab.add(tempComb);
+					}					
+				}
+			}
+			combTab = nextCombTab;
+		}
+		if (this.solutions == null)
+		{
+			System.out.println("Impossible de former la somme de " + total);
+		}
+		else
+		{
+			System.out.println(this.solutions.size() + " possibilités différentes de former la somme de " + total);
+		}	
+	}
+	
+	private class Combination 
+	{
+		int sum;
+		int maxIndex;
+		List<Integer> array;
+				
+		public Combination(int coinIndex)
+		{
+			this.array = new ArrayList<Integer>();
+			int coin = coins.get(coinIndex);
+			this.array.add(coin);
+			this.sum = coin;
+			this.maxIndex = coinIndex;
+		}
+		
+		public Combination(Combination comb)
+		{
+			this.sum = comb.sum;
+			this.maxIndex = comb.maxIndex;
+			this.array = new ArrayList<Integer>(comb.array);
+		}
+		
+		public void add(int coinIndex){
+			int coin = coins.get(coinIndex);
+			this.array.add(coin);
+			this.sum += coin;
+			if(coinIndex > this.maxIndex)
+				this.maxIndex = coinIndex;
+			
+		}
+	}
+}
